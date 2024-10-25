1534150e1497f5227cb1a6eff1b065b8a4376b95
MaximeWeb <neyret.maxime@gmail.com>
2024-10-25 17:48:25 +0200

    FinalCommit

--- modal.js
+++ modal.js
@@ -28,19 +28,16 @@ const locations = form.querySelectorAll('input[name="location"]');
 // Btn
 
 modalBtn.addEventListener("click", () => {
-  // faire apparaitre le block formulaire au click du modalBtn
   modalbg.style.display = "block";
 });
 
 closeBtn.addEventListener("click", () => {
-  // On ferme la modal avec la croix
   modalbg.style.display = "none";
   form.reset();
   hideAllErrors();
 });
 
 closeValidated.addEventListener("click", () => {
-  // On ferme la modal avec la croix
   validated.style.display = "none";
   modalbg.style.display = "none";
   content.classList.remove("hidden");
@@ -49,14 +46,12 @@ closeValidated.addEventListener("click", () => {
 // function
 
 function showError(input, message) {
-  // fonction afin de faire apparaitre les messages d'erreur avec la classe css formData
-  const formData = input.closest(".formData"); // on va chercher l'element le plus proche du parent formData , donc les input .
-  formData.setAttribute("data-error", message); // setAttribute afin de recuperer l'attribut data-error + ajouté un message d'erreur
-  formData.setAttribute("data-error-visible", "true"); // ici on va faire apparaitre l'emplacement du message d'erreur
+  const formData = input.closest(".formData");
+  formData.setAttribute("data-error", message);
+  formData.setAttribute("data-error-visible", "true");
 }
 
 function hideError(input) {
-  // On va gérer le cas ou l'utilisateur est revenu sur le champ afin de modifier son erreur
   const formData = input.closest(".formData");
   formData.setAttribute("data-error-visible", "false");
   formData.setAttribute("data-error", "");
@@ -64,34 +59,30 @@ function hideError(input) {
 
 function hideAllErrors() {
   formDataElements.forEach((formData) => {
-    formData.setAttribute("data-error-visible", "false"); // Cache le message d'erreur
-    formData.setAttribute("data-error", ""); // Efface le texte du message d'erreur
+    formData.setAttribute("data-error-visible", "false");
+    formData.setAttribute("data-error", "");
   });
 }
 
 const validate = (event) => {
-  event.preventDefault(); // PreventDefault pour stop l'evenement de fermeture par default du formulaire
-  let isValid = true; // Création d'un etat si valide ou pas
+  event.preventDefault();
+  let isValid = true;
 
-  const nameRegex = /^[a-zA-ZÀ-ÿ '-]{2,}$/; // regex de nom qui oblige a utiliser minimum deux caractere
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // regex pour un format d'email valide
+  const nameRegex = /^[a-zA-ZÀ-ÿ '-]{2,}$/;
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-  const formData = new FormData(form); // Je crée un nouvelle objet iterable FormData qui aura comme param Key,Value
-  // const data = Object.fromEntries(formData.entries());   // Grace a .entries j'obtiens un iterateur et fromEntries pour le changer en objet js lisible
-  // console.log(data)
+  const formData = new FormData(form);
+  // const data = Object.fromEntries(formData.entries());
+  // console.log(data);
 
   for (const [key, value] of formData.entries()) {
-    // for of pour agir sur plusieur champs sous forme de boucle avec les param key,value
-    switch (
-      key // switch pour eviter les répétition if if if avec le param key
-    ) {
-      case "first": // case qui fonctionne avec switch " dans le cas ou la clef s'apelle first ..ect"
+    switch (key) {
+      case "first":
         if (!nameRegex.test(value.trim())) {
-          // .trim pour que les espace ne soit pas compter comme des characteres , .test methode de verification du regex
           showError(
             firstName,
             "Veuillez entrer un prénom valide (2 caractères minimum)."
-          ); // fonction showError + input + message
+          );
           isValid = false;
         } else {
           hideError(firstName);
@@ -120,7 +111,7 @@ const validate = (event) => {
         break;
 
       case "birthdate":
-        const birthDateValue = new Date(value); // crée une constante qui prend un format de date , et getTime pour verifié que la date est bien valide (+1970)
+        const birthDateValue = new Date(value);
         if (value === "" || isNaN(birthDateValue.getTime())) {
           showError(birthdate, "Veuillez entrer une date de naissance valide.");
           isValid = false;
@@ -129,7 +120,7 @@ const validate = (event) => {
         }
         break;
 
-      case "quantity": // Gere le cas ou c'est bien un number entier.
+      case "quantity":
         if (value.trim() === "" || isNaN(value) || parseInt(value) < 0) {
           showError(
             quantity,
@@ -146,21 +137,17 @@ const validate = (event) => {
     }
   }
 
-  // j'utilise Array.from location( mes boutons radios ) pour les intégrer dans un tableau et avec .some je verifie que au moin 1 data du tableau est checked
-
   const locationChecked = Array.from(locations).some(
     (button) => button.checked
   );
-  console.log(locationChecked);
   if (!locationChecked) {
-    showError(locations[0], "Veuillez sélectionner une localisation."); // On vien visé un element du tableau pour affiché mon message d'erreur
+    showError(locations[0], "Veuillez sélectionner une localisation.");
     isValid = false;
   } else {
     hideError(locations[0]);
   }
 
   if (!conditions.checked) {
-    // ici on gere la case conditions qui a comme indication html "checked" , si la case n'est pas checked , error.
     showError(conditions, "Vous devez accepter les conditions d'utilisation.");
     isValid = false;
   } else {
@@ -168,7 +155,6 @@ const validate = (event) => {
   }
 
   if (isValid) {
-    // alert("Formulaire soumis avec succès !");
     form.reset();
     content.classList.add("hidden");
     validated.style.display = "block";
